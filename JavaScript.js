90f70313ebd348b4cb63c0cb7ef500953d1802ed
apprenticekate <108842390+apprenticekate@users.noreply.github.com>
2022-12-16 18:07:21 -0500

    tidying code
    edited code to reduce redundancy and make it easier to read; removed unnecessary photos

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,11 @@
-//home button
+//return to quiz
 const toHome = () => window.location.href='Quiz.html';
+
+//return to guide 
+const toGuide = () => window.location.href='Houseplants.html';
+
+//return to top
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'}); 
 
 //calculate user results
 function result(){
@@ -18,7 +24,3 @@ function result(){
     else if (document.getElementById('someSun').checked && document.getElementById('noPets').checked) {
       window.location.href='Pothos.html';    
     }}
-
-
-    /* take user back to top
-const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'}); */
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Houseplants
 A catalog of Kate's current houseplants and their optimal care conditions.
 
-This catalog serves as a reference for:
-- Kate as she moves plants around her house; and/or 
-- Anyone in North America's humid subtropics interested in caring for beginner-friendly houseplants. 
+Also, a quiz to match beginners to their ideal starter plant.
